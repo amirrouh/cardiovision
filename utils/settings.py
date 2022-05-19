c27a870be235fe607b9e4c52a43a9d567861c72a
root <root@DESKTOP-R7J3PT8.localdomain>
2022-05-19 17:48:03 -0400

    lv component

--- utils/settings.py
+++ utils/settings.py
@@ -5,7 +5,7 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 working_dir = Path(os.path.join(this_directory, ".."))
 sys.path.append(working_dir)
 SECRET_KEY = os.environ.get("AM_I_IN_A_DOCKER_CONTAINER", False)
-component = "aorta"
+component = "lv"
 input_file = "/home/data/input_file.nrrd"
 output_dir = "/home/data/cardiovision_results"
 verbose = False
