dbf96b0f3e8da5d0935f676036f58c38bcbd5265
Amir Rouhollahi <43451887+amirrouh@users.noreply.github.com>
2022-12-29 17:56:34 -0500

    Delete webapp directory

--- webapp/gui/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.gui_home, name='gui_home'),
-    path('visualization', views.gui_visualization, name="gui_visualization"),
-]
