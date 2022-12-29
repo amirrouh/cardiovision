1786cb57abf8d53f9010dbaa3a9997f36d5838b4
Amir Rouhollahi <43451887+amirrouh@users.noreply.github.com>
2022-12-29 17:57:13 -0500

    Update cardiovision.sh
    new component added

--- scripts/cardiovision.sh
+++ scripts/cardiovision.sh
@@ -10,7 +10,7 @@ case $1 in
     -i) # initialization (resampling and data generation)
     eval "$(conda shell.bash hook)"
     conda activate training
-    python scripts/generate_settings.py
+    python scripts/generate_settings.py -new
     python cnn/src/preprocess/1_resample.py
     python cnn/src/preprocess/2_split.py
     python cnn/src/preprocess/3_datagen.py
@@ -18,7 +18,7 @@ case $1 in
     -t)
     eval "$(conda shell.bash hook)"
     conda activate cnn
-    python scripts/generate_settings.py 
+    python scripts/generate_settings.py -new
     python cnn/src/train/1_train.py
     ;;  
     *)
