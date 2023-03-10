d795847a45540ee32df9b89db28ba40c52b9c45c
sdyhaltmeier <sdyhaltmeier@W0273832.partners.org>
2023-03-10 11:03:26 -0500

    fixed bug ui/config in ui/config.py

--- ui/config.py
+++ ui/config.py
@@ -1,6 +1,6 @@
-output_directory = "/mnt/d/amir/cardiovision_results"
-training_data_directory = "/mnt/d/data/segmentations/aorta_segmentations/"
-input_file = "/mnt/d/data/segmentations/aorta_segmentations/images/ct1.nrrd"
+output_directory = "/mnt/c/Users/SH1389/Documents/cardiovision_results"
+training_data_directory = "/mnt/c/Users/SH1389/Documents/data/segmentations/aorta_segmentations/"
+input_file = "/mnt/c/Users/SH1389/Documents/data/segmentations/aorta_segmentations/images/ct1.nrrd"
 
 # component can be "aorta"; "lv"
 component = "aorta"
